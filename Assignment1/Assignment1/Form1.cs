bb807b3ad01ca2e296f800281590609b6366c1a7
GiaHuyhe187192 <huynghe187192@fpt.edu.vn>
2026-01-30 16:24:50 +0700

    Add second button show message 'tạm biệt'

--- Assignment1/Assignment1/Form1.cs
+++ Assignment1/Assignment1/Form1.cs
@@ -11,5 +11,10 @@ namespace Assignment1
         {
             MessageBox.Show("xin chào");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("tạm biệt");
+        }
     }
 }
