40b09f8927a21e89222fe84cd601c71554472a5e
GiaHuyhe187192 <huynghe187192@fpt.edu.vn>
2026-01-30 16:14:30 +0700

    Add button show message 'xin chào'

--- Assignment1/Assignment1/Form1.cs
+++ Assignment1/Assignment1/Form1.cs
@@ -6,5 +6,10 @@ namespace Assignment1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("xin chào");
+        }
     }
 }
